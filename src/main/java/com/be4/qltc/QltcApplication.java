35e5b9028add02c988ed1aa7a578f7d349036047
Le Thanh Long <longlt201203@gmail.com>
2025-01-21 09:11:38 +0700

    init

---
+++ src/main/java/com/be4/qltc/QltcApplication.java
@@ -0,0 +1,13 @@
+package com.be4.qltc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QltcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QltcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be4/qltc/QltcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be4.qltc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QltcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
